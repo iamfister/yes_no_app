3e8bbcc1f7a6417d617580905bf06d884c8a8b58
fister <fistergutierrez@gmail.com>
2023-11-22 13:28:02 -0600

    ListView

--- lib/presentation/chat_screen.dart
+++ lib/presentation/chat_screen.dart
@@ -7,14 +7,42 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: const Padding(
-            padding: EdgeInsets.all(3.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage('https://a.storyblok.com/f/178900/960x539/1a76b940d8/ado-the-silent-service-mv.jpg/m/filters:quality(95)format(webp)'),
-            ),
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://a.storyblok.com/f/178900/960x539/1a76b940d8/ado-the-silent-service-mv.jpg/m/filters:quality(95)format(webp)'),
           ),
-          title: const Text('ADO bb'),
-        ),      
+        ),
+        title: const Text('ADO bb'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice: $index');
+                },
+              )
+            ),
+            const Text('Hola bb')
+          ],
+        ),
+      ),
     );
   }
 }
